4ac5bcbc284bd052d32b8029e74e10a9dc30e23d
gyun_ky <kiyoon9390@gmail.com>
2024-01-21 23:59:10 +0900

    feat : first commit

---
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/LetMeDoWithApplication.java
@@ -0,0 +1,13 @@
+package com.LetMeDoWith.LetMeDoWith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetMeDoWithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetMeDoWithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LetMeDoWith/LetMeDoWith/LetMeDoWithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LetMeDoWith.LetMeDoWith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetMeDoWithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
