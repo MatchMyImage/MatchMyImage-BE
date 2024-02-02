d835af10e32ac849e14e8b71e231170e73e3391f
SJ Kim <hedro0458@gmail.com>
2024-02-02 18:24:15 +0900

    fix: status가 잘못 분류되어 있던 것을 수정함

--- src/main/java/com/LetMeDoWith/LetMeDoWith/common/ApiResponseFailStatus.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/common/ApiResponseFailStatus.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ApiResponseFailStatus {
     BAD_REQUEST("BAD_REQUEST", "BAD_REQUEST", HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 에러입니다. 다시 시도하세요."),
-    OBJECT_NOT_FOUND("OBJECT_NOT_FOUND", "OBJECT_NOT_FOUND", HttpStatus.OK, "조회 결과가 존재하지 않습니다."),
+    NOT_FOUND("NOT_FOUND", "NOT_FOUND", HttpStatus.NOT_FOUND, "존재하지 않는 API Endpoint입니다."),
     INVALID_PARAM_ERROR("INVALID_PARAM_ERROR", "INVALID_PARAM_ERROR", HttpStatus.BAD_REQUEST, "요청 파라미터가 잘못되었습니다."),
     TOKEN_EXPRIED("TOKEN_EXPRIED", "TOKEN_EXPRIED", HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     UNAUTHORIZED("UNAUTHORIZED", "UNAHTHORIZED", HttpStatus.UNAUTHORIZED, "접근 권한이 없습니다."),
--- src/main/java/com/LetMeDoWith/LetMeDoWith/common/ApiResponseSuccessStatus.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/common/ApiResponseSuccessStatus.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ApiResponseSuccessStatus {
     OK("OK", "OK", HttpStatus.OK, "성공적으로 조회하였습니다."),
-    NOT_FOUND("NOT_FOUND", "NOT_FOUND", HttpStatus.NOT_FOUND, "존재하지 않는 API Endpoint입니다.");
+    OBJECT_NOT_FOUND("OBJECT_NOT_FOUND", "OBJECT_NOT_FOUND", HttpStatus.OK, "조회 결과가 존재하지 않습니다.");
 
 
     private String statusName;
