f1a83d19ff9e0f706c2f53e87b9cc6ed7368f1ca
SJ Kim <hedro0458@gmail.com>
2024-02-24 21:05:55 +0900

    feat: base audit 객체 생성

--- src/main/java/com/LetMeDoWith/LetMeDoWith/LetMeDoWithApplication.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/LetMeDoWithApplication.java
@@ -2,12 +2,14 @@ package com.LetMeDoWith.LetMeDoWith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LetMeDoWithApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LetMeDoWithApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LetMeDoWithApplication.class, args);
+    }
 
 }
