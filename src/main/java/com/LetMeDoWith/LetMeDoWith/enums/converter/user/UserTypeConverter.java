a72d4ccd12562ef4b9eaf48c3894742004c811f4
SJ Kim <hedro0458@gmail.com>
2024-03-02 01:01:07 +0900

    feat: AbstractAttributeConverter 구현

--- src/main/java/com/LetMeDoWith/LetMeDoWith/enums/converter/user/UserTypeConverter.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/enums/converter/user/UserTypeConverter.java
@@ -1,23 +1,8 @@
 package com.LetMeDoWith.LetMeDoWith.enums.converter.user;
 
 import com.LetMeDoWith.LetMeDoWith.enums.user.UserType;
-import com.LetMeDoWith.LetMeDoWith.util.EnumUtil;
-import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter
-public class UserTypeConverter implements AttributeConverter<UserType, String> {
-    @Override
-    public String convertToDatabaseColumn(UserType attribute) {
-        return attribute.getCode();
-    }
-
-    @Override
-    public UserType convertToEntityAttribute(String dbData) {
-        try {
-            return EnumUtil.getEnum(UserType.class, dbData);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+public class UserTypeConverter extends AbstractAttributeConverter<UserType> {
 }
