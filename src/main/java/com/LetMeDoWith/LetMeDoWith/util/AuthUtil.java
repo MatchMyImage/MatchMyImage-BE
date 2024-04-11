12877efc29bb550806d1a2dd08413da52d1f58f1
SJ Kim <hedro0458@gmail.com>
2024-04-11 15:37:43 +0900

    feat: OIDC ID Token 검증 로직 및 테스트

--- src/main/java/com/LetMeDoWith/LetMeDoWith/util/AuthUtil.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/util/AuthUtil.java
@@ -1,40 +1,157 @@
 package com.LetMeDoWith.LetMeDoWith.util;
 
+import com.LetMeDoWith.LetMeDoWith.enums.common.FailResponseStatus;
+import com.LetMeDoWith.LetMeDoWith.exception.RestApiException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletRequest;
+import java.math.BigInteger;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.LetMeDoWith.LetMeDoWith.enums.common.FailResponseStatus;
-import com.LetMeDoWith.LetMeDoWith.exception.RestApiException;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.experimental.UtilityClass;
-
 @UtilityClass
+@Slf4j
 public class AuthUtil {
-
-	private final String AUTHORIZATION_KEY = "AUTHORIZATION";
-
-	public String getAccessToken() {
-		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
-		String authorizationHeader = request.getHeader(AUTHORIZATION_KEY);
-		if (authorizationHeader == null) {
-			throw new RestApiException(FailResponseStatus.ATK_NOT_EXIST);
-		}
-		if (!authorizationHeader.startsWith("Bearer")) {
-			throw new RestApiException(FailResponseStatus.ATK_NOT_EXIST);
-		}
-		String accessToken = authorizationHeader.substring(6);
-		if (accessToken.isEmpty()) {
-			throw new RestApiException(FailResponseStatus.ATK_NOT_EXIST);
-		} else {
-			return accessToken;
-		}
-	}
-
-	public Long getMemberId() {
-
-		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
-		return (Long) request.getAttribute("memberId");
-
-	}
-
+    
+    private final String AUTHORIZATION_KEY = "AUTHORIZATION";
+    private final String KID = "kid";
+    
+    public String getAccessToken() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String authorizationHeader = request.getHeader(AUTHORIZATION_KEY);
+        if (authorizationHeader == null) {
+            throw new RestApiException(FailResponseStatus.ATK_NOT_EXIST);
+        }
+        if (!authorizationHeader.startsWith("Bearer")) {
+            throw new RestApiException(FailResponseStatus.ATK_NOT_EXIST);
+        }
+        String accessToken = authorizationHeader.substring(6);
+        if (accessToken.isEmpty()) {
+            throw new RestApiException(FailResponseStatus.ATK_NOT_EXIST);
+        } else {
+            return accessToken;
+        }
+    }
+    
+    public Long getMemberId() {
+        
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return (Long) request.getAttribute("memberId");
+        
+    }
+    
+    /**
+     * Unsigned token에서 kid를 가져온다,
+     *
+     * @param token Unsigned ID token
+     * @param aud   자격증명 제공자에서 발급한 어플리케이션 아이디
+     * @param iss   자걱증명 제공자의 url
+     * @return ID token의 kid 값
+     */
+    public String getKidFromUnsignedTokenHeader(String token, String aud, String iss) {
+        return (String) getUnsignedTokenClaims(token, aud, iss).getHeader().get(KID);
+    }
+    
+    /**
+     * ID token의 서명을 검증하고, parsing한다.
+     *
+     * @param token    ID token
+     * @param modulus
+     * @param exponent
+     * @return parsing된 id token
+     */
+    public Jws<Claims> verifyOidcToken(String token, String modulus, String exponent) {
+        
+        try {
+            return Jwts.parserBuilder()
+                       .setSigningKey(getRSAPublicKey(modulus, exponent))
+                       .build()
+                       .parseClaimsJws(token);
+        } catch (SignatureException e) {
+            throw new IllegalArgumentException("잘못된 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("만료된 토큰입니다.");
+        } catch (Exception e) {
+            log.error(e.toString());
+            throw new RuntimeException("Token을 파싱하는 도중 에러가 밣생했습니다.");
+        }
+    }
+    
+    /**
+     * Encoding된 Signed-token 에서 signature를 분리한다.
+     *
+     * @param signedToken signature를 포함하는 token 전문
+     * @return signature가 제거된 token
+     */
+    private String getUnsignedToken(String signedToken) {
+        String[] splitToken = signedToken.split("\\.");
+        
+        if (splitToken.length != 3) {
+            throw new IllegalArgumentException("올바르지 않은 Token입니다!");
+        }
+        
+        return splitToken[0] + '.' + splitToken[1] + '.';
+    }
+    
+    /**
+     * OIDC ID Token의 iss, aud, 만료일자를 검증하고, parse한다.
+     *
+     * @param token ID Token. Signature가 제거되있어야 한다.
+     * @param aud   자격증명 제공자에서 발급한 어플리케이션 아이디
+     * @param iss   자걱증명 제공자의 url
+     * @return parsing된 ID Token.
+     */
+    private Jwt<Header, Claims> getUnsignedTokenClaims(String token, String aud, String iss) {
+        try {
+            return Jwts.parserBuilder()
+                       .requireAudience(aud)  // aud(provider에 지정한 어플리케이션 아이디) 가 같은지 확인
+                       .requireIssuer(iss) // iss(issuer)가 알맞은 provider인지 확인
+                       .build()
+                       .parseClaimsJwt(getUnsignedToken(token));
+        } catch (ExpiredJwtException e) { // 파싱하면서 만료된 토큰인지 확인.
+            throw new IllegalStateException("토큰이 만료되었습니다.");
+        } catch (SignatureException e) {
+            throw new RuntimeException("잘못된 서명입니다.");
+        } catch (Exception e) {
+            log.error(e.toString());
+            throw new RuntimeException("Token을 파싱하는 도중 에러가 밣생했습니다.");
+        }
+    }
+    
+    /**
+     * n, e로 public key를 계산한다.
+     *
+     * @param modulus
+     * @param exponent
+     * @return 계산된 public key
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     */
+    private Key getRSAPublicKey(String modulus, String exponent)
+        throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        
+        byte[] decodeN = Base64.getUrlDecoder().decode(modulus);
+        byte[] decodeE = Base64.getUrlDecoder().decode(exponent);
+        
+        BigInteger n = new BigInteger(1, decodeN);
+        BigInteger e = new BigInteger(1, decodeE);
+        
+        RSAPublicKeySpec keySpec = new RSAPublicKeySpec(n, e);
+        return keyFactory.generatePublic(keySpec);
+    }
+    
 }
---
+++ src/test/java/com/LetMeDoWith/LetMeDoWith/util/AuthUtilTest.java
@@ -0,0 +1,124 @@
+package com.LetMeDoWith.LetMeDoWith.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import java.time.Instant;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class AuthUtilTest {
+    
+    /*
+     * Sample OIDC ID Tokens for test.
+     * You cannot utilize this token for any purpose on this system.
+     */
+    
+    public final String SAMPLE_TOKEN_NORMAL = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjllMjQxM2UzODI1YWMyYmFiMTdmZTRkNGJhZDkxMjhjIn0.eyJpc3MiOiJodHRwczovL21vY2suYXV0aGVudGljYXRlLmxldG1lZG93aXRoLmRvIiwiYXVkIjoibGV0bWVkb3dpdGhTYW1wbGVBcHBsaWNhdGlvbiIsInN1YiI6IjViZTg2MzU5MDczYzQzNGJhZDJkYTM5MzIyMjJkYWJlIiwiZXhwIjoxOTk5OTk5OTk5LCJpYXQiOjE3MTI1NjM1MDMsIm5pY2tuYW1lIjoiSm9uaCBEb2UiLCJlbWFpbCI6ImpkMTIzQGxldG1lZG93aXRoLmNvbSJ9.zJMubgT5utETNIaUjuAkoRC74-eyPjVOaiiYIFlzv3eVOrbj7bM7RxpZtFeiYdvmpyiEH7sHbo-qVnTukeLlTaT_wNDeLSS7GYskbC75T_zCGx4e5vt4fgDTI4ov_iVWq9RzawTwFzJqj0b3j9hNST4HkuizQymAt5tnFClEa2fE_ojikecBEhfqdn3PAgOzq-pcHM-7-q1cs0QWrseFeouGvVmsOoiRTyewQ8ouuCf9zaYIf6KUu-lAQPsmsjYmNI2MHlPLCZbnnp3qZo7R-ozX66ZELbZwVZxPvrLqLOjdS4yPk_-aaQhT9s5sB0SljmWh9LLYELyXXkiQbVkx9QU-W0TjgnDpb-ScLQ2C5NXgEAF6OHBGynjyymwi_rvBxfY4l22evluOzBp18ECpc7DOhlcnwrPrS5M4pSCpEd8CLlYTyNvYWBJ7umhhDcub5ZLh6cqZ2LJanDG-UM1nLxRB6QGOFZcazyR1rPeGb7kiXiOh6-E7Wz7zwKvhcAUB";
+    public final String SAMPLE_TOKEN_ILLEGAL_SIGNATURE = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjllMjQxM2UzODI1YWMyYmFiMTdmZTRkNGJhZDkxMjhjIn0.eyJpc3MiOiJodHRwczovL21vY2suYXV0aGVudGljYXRlLmxldG1lZG93aXRoLmRvIiwiYXVkIjoibGV0bWVkb3dpdGhTYW1wbGVBcHBsaWNhdGlvbiIsInN1YiI6IjViZTg2MzU5MDczYzQzNGJhZDJkYTM5MzIyMjJkYWJlIiwiZXhwIjoxNzEyNTYzNTAzLCJpYXQiOjE5OTk5OTk5OTksIm5pY2tuYW1lIjoiSm9uaCBEb2UiLCJlbWFpbCI6ImpkMTIzQGxldG1lZG93aXRoLmNvbSJ9.TNM7fBiE7joA5FtMbRB4rrjijg2ctngwSnjFXcjfRByeCos9YewaL9QWiHz0uWJodZB6355ZdEYHaFvoWkGaKe1rbGQ75VRO6b8nqheeNRmed2ZajEZRDUSl5zvKuV49yuDo3sFjwEWHHYJBKpYFbnxJt_rmeWitLhxxfOJomnzdxu5hYY6IAeqLgdt-sfxqNbqziyjMQr6AG3jqvDEasrGSKoiTILnJXpK3paf7I3PRAKVFitaXc8S8ddP0QNrEt2_aqTjJbpSnYiV9OoxgacSJo4c_SKeHKbONis1jjubYAu083T0zH6QZMkfey7rIDmhIIhNpugIpp5-QRtmZ6p_P4-aYZPioLcz-U-rvGlQENwyBTds7-_diafSKRQvHpX7Vzdvin-ZlOeXQDoLchT7wZYNWL6WZv9WcsHZvw1K2NjCvCKb09sPup3YaiTfqze46pxej3lhL5SgXKXQfPEyisdggx2VZgHCnBocGRk60S0OkgBgjX9HZHAYzwMcGAnH";
+    public final String SAMPLE_TOKEN_MALFORMED = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjllMjQxM2UzODI1YWMyYmFiMTdmZTRkNGJhZDkxMjhjIn0.eyJpc3MiOiJodHRwczovL21vY2suYXV0aGVudGljYXRlLmxldG1lZG93aXRoLmRvIiwiYXVkIjoibGV0bWVkb3dpdGhTYW1wbGVBcHBsaWNhdGlvbiIsInN1YiI6IjViZTg2MzU5MDczYzQzNGJhZDJkYTM5MzIyMjJkYWJlIiwiZXhwIjoxNzEyNTYzNTAzLCJpYXQiOjE5OTk5OTk5OTksIm5pY2tuYW1lIjoiSm9uaCBEb2UiLCJlbWFpbCI6ImpkMTIzQGxldG1lZG93aXRoLmNvbSJ9.TNM7fBiE7joA5FtMbRB4rrjijg2ctngwSnjFXcjfRByeCos9YewaL9QWiHz0uWJodZB6355ZdEYHaFvoWkGaKe1rbGQ75VRO6b8nqheeNRmed2ZajEZRDUSl5zvKuV49yuDo3sFjwEWHHYJBKpYFbnxJt_rmeWitLhxxfOJomnzdxu5hYY6IAeqLgdt-sfxqNbqziyjMQr6AG3jqvDEasrGSKoiTILnJXpK3paf7I3PRAKVFitaXc8S8ddP0QNrEt2_aqTjJbpSnYiV9OoxgacSJo4c_SKeHKbONis1jjubYAu083T0zH6QZMkfey7rIDmhIIhNpugIpp5-QRtmZ6p_P4-aYZPioLcz-U-rvGlQENwyBTds7-_diafSKRQvHpX7Vzdvin-ZlOeXQDoLchT7wZYNWL6WZv9WcsHZvw1K2NjCvCKb09sPup3YaiTfqze46pxej3lhL5SgXKXQfPEyigx2VZgHCnB.ocGRk60S0OkgBgjX9HZHAYzwMcGAnH";
+    public final String SAMPLE_TOKEN_EXPIRED = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjllMjQxM2UzODI1YWMyYmFiMTdmZTRkNGJhZDkxMjhjIn0.eyJpc3MiOiJodHRwczovL21vY2suYXV0aGVudGljYXRlLmxldG1lZG93aXRoLmRvIiwiYXVkIjoibGV0bWVkb3dpdGhTYW1wbGVBcHBsaWNhdGlvbiIsInN1YiI6IjViZTg2MzU5MDczYzQzNGJhZDJkYTM5MzIyMjJkYWJlIiwiZXhwIjoxNzEyNTYzNTAzLCJpYXQiOjE3MTI1NjM1MDMsIm5pY2tuYW1lIjoiSm9uaCBEb2UiLCJlbWFpbCI6ImpkMTIzQGxldG1lZG93aXRoLmNvbSJ9.wnJAIys4Bj5-3JZy-BLKDL_tny7JESfqgO5qBKTtYfMPChMEhF8Biq9njGipZDYLfg71d3EiFqLcyUZdL5yuyesuygRJuYtydtdZbmD1Fsqo1UC0dfUTrCZqfIC0TrfrltJub3OYiFSvpYiz27mWsM-TaWp5x3e9z94LoOtU8EtAwASTF2YJ5e_Erla0ucqMmlD9RuuDc-xUj0SAlH8sIe5OqivCfCKfVrEX4AGYInZzuDfH_GyjgfUo1x_dkHnQIH7pq49-HZtZKHf6dMhIxENTrazOY1Q-P1a4l17T09KD1hVO4iXYNCtM5LcoksPo5wXE0GbcwmE_2Tg3N6MniyVXJBsdh4KyyPovykdNiVxu2qy73iGqwZRahH35lDz6F879NCkXuBoLoU-LpGHSIlw3IqpE__zp03VO_wJtXRynV-uFNg99boaU1k24lHbFXd_nMI1nu04trokuwCamYxIL_Kz8y3MHoK-VLyaCpHXR90uax_we1niH4hG0h_ms";
+    
+    public final String SAMPLE_AUD = "letmedowithSampleApplication";
+    public final String SAMPLE_KID = "9e2413e3825ac2bab17fe4d4bad9128c";
+    public final String SAMPLE_ISS = "https://mock.authenticate.letmedowith.do";
+    public final Long SAMPLE_EXPIRE = 1999999999L;
+    
+    public final String SAMPLE_MOD = "zhMyuF42t7vy2VjnXj2pI2kssakfgaNJqtBqKkh_IBidqKTIM2mEejJ-b0HUwgQ0YzyZGA1OixLxvWuRTrY3j9RXPg0wj7J7e7TkPqZ83sMQ7lUqfzHfR4mMJQ9Si33CFSm8pBkJt38QS9ciLb-uf2cg9N-GSo1e6YAiywlc-w5UOW9Ur_2N5OeHQAWJM1V7LxSbJEakGJG_ivrghrLfh9h-VaYcvfyCJnbkcHGtpubH7LSo5a80_-S9hkvoHuhow27w9mxLm0K4IR1N8BmJbIBc19pMm8i-BQouHL0tbOr0-843GpoidCsXsk-jL9Egqmp9W3qA_WDU6Ra_SFJzFmbC6lqWveUYcKIh7h-qjpkwWrU_88kO5WuX0QiyV4VDj_uRhbtkMxzKWC-QVFGOhG5h2FJnC1lL1lQaIPa5KfxcxpptThLho1NKkgQoblItidMb3rxHdxMrWHVMkvgPhbN2Z5Yb3zo0Yxa9Svbh0n73iTB2GNrdM8q8EC12abHZ";
+    public final String SAMPLE_EXPONENT = "AQAB";
+    
+    @Test
+    @DisplayName("[SUCCESS] Token signature 추출 테스트")
+    void extractSignatureFromNormalTokenTest() {
+        String kid = AuthUtil.getKidFromUnsignedTokenHeader(SAMPLE_TOKEN_NORMAL,
+                                                            SAMPLE_AUD,
+                                                            SAMPLE_ISS);
+        
+        assertEquals(kid, SAMPLE_KID);
+    }
+    
+    @Test
+    @DisplayName("[SUCCESS] ID token 검증 테스트")
+    void verifyNormalTokenTest() {
+        String modulus = "zhMyuF42t7vy2VjnXj2pI2kssakfgaNJqtBqKkh_IBidqKTIM2mEejJ-b0HUwgQ0YzyZGA1OixLxvWuRTrY3j9RXPg0wj7J7e7TkPqZ83sMQ7lUqfzHfR4mMJQ9Si33CFSm8pBkJt38QS9ciLb-uf2cg9N-GSo1e6YAiywlc-w5UOW9Ur_2N5OeHQAWJM1V7LxSbJEakGJG_ivrghrLfh9h-VaYcvfyCJnbkcHGtpubH7LSo5a80_-S9hkvoHuhow27w9mxLm0K4IR1N8BmJbIBc19pMm8i-BQouHL0tbOr0-843GpoidCsXsk-jL9Egqmp9W3qA_WDU6Ra_SFJzFmbC6lqWveUYcKIh7h-qjpkwWrU_88kO5WuX0QiyV4VDj_uRhbtkMxzKWC-QVFGOhG5h2FJnC1lL1lQaIPa5KfxcxpptThLho1NKkgQoblItidMb3rxHdxMrWHVMkvgPhbN2Z5Yb3zo0Yxa9Svbh0n73iTB2GNrdM8q8EC12abHZ";
+        String exponent = "AQAB";
+        
+        Jws<Claims> oidcToken = AuthUtil.verifyOidcToken(SAMPLE_TOKEN_NORMAL, modulus, exponent);
+        
+        assertEquals(oidcToken.getBody().getIssuer(), SAMPLE_ISS);
+        assertEquals(oidcToken.getBody().getExpiration(),
+                     Date.from(Instant.ofEpochSecond(SAMPLE_EXPIRE)));
+    }
+    
+    @Test
+    @DisplayName("[FAIL] 만료된 token에서 signature 추출 시도 ")
+    void extractSignatureFromExpiredTokenTest() {
+        assertThrows(IllegalStateException.class,
+                     () -> AuthUtil.getKidFromUnsignedTokenHeader(SAMPLE_TOKEN_EXPIRED,
+                                                                  SAMPLE_AUD,
+                                                                  SAMPLE_ISS),
+                     "토큰이 만료되었습니다.");
+    }
+    
+    @Test
+    @DisplayName("[FAIL] 잘못된 signature 가지는 token에서 signature 추출 시도 ")
+    void extractSignatureFromIllegalSignatureTokenTest() {
+        assertThrows(RuntimeException.class,
+                     () -> AuthUtil.getKidFromUnsignedTokenHeader(SAMPLE_TOKEN_ILLEGAL_SIGNATURE,
+                                                                  SAMPLE_AUD,
+                                                                  SAMPLE_ISS),
+                     "잘못된 서명입니다.");
+    }
+    
+    @Test
+    @DisplayName("[FAIL] 잘못된 format 가지는 token에서 signature 추출 시도 ")
+    void extractSignatureFromMalformedTokenTest() {
+        assertThrows(RuntimeException.class,
+                     () -> AuthUtil.getKidFromUnsignedTokenHeader(SAMPLE_TOKEN_MALFORMED,
+                                                                  SAMPLE_AUD,
+                                                                  SAMPLE_ISS),
+                     "Token을 파싱하는 도중 에러가 밣생했습니다.");
+    }
+    
+    @Test
+    @DisplayName("[FAIL] 만료된 ID token 검증 시도")
+    void verifyExpiredTokenTest() {
+        assertThrows(RuntimeException.class,
+                     () -> AuthUtil.verifyOidcToken(SAMPLE_TOKEN_EXPIRED,
+                                                    SAMPLE_MOD,
+                                                    SAMPLE_EXPONENT),
+                     "만료된 토큰입니다.");
+    }
+    
+    @Test
+    @DisplayName("[FAIL] 잘못된 signature 가지는 ID token 검증 시도")
+    void verifyIllegalSignatureTokenTest() {
+        assertThrows(IllegalArgumentException.class,
+                     () -> AuthUtil.verifyOidcToken(SAMPLE_TOKEN_ILLEGAL_SIGNATURE,
+                                                    SAMPLE_MOD,
+                                                    SAMPLE_EXPONENT));
+    }
+    
+    @Test
+    @DisplayName("[FAIL] 잘못된 format 가지는 ID token 검증 시도")
+    void verifyMalformedTokenTest() {
+        assertThrows(RuntimeException.class,
+                     () -> AuthUtil.verifyOidcToken(SAMPLE_TOKEN_MALFORMED,
+                                                    SAMPLE_MOD,
+                                                    SAMPLE_EXPONENT));
+    }
+    
+    @Test
+    @DisplayName("[FAIL] 잘못된 RSA Key로 ID token 검증 시도")
+    void verifyTokenWithInvalidKeyTest() {
+        assertThrows(RuntimeException.class,
+                     () -> AuthUtil.verifyOidcToken(SAMPLE_TOKEN_NORMAL,
+                                                    "SAMPLE_MOD",
+                                                    "SAMPLE_EXPONENT"));
+    }
+}
