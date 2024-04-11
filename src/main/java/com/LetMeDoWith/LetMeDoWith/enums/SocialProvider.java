d971ab69d649b51d672317627a344e5e4f8f5544
SJ Kim <hedro0458@gmail.com>
2024-04-11 15:20:00 +0900

    update: SocialProvider 스펙 변경: AUD 필드 삭제; Service에서 불러오도록 변경.

--- src/main/java/com/LetMeDoWith/LetMeDoWith/enums/SocialProvider.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/enums/SocialProvider.java
@@ -8,16 +8,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SocialProvider implements BaseEnum {
     
-    KAKAO("KAKAO", "카카오", "https://kauth.kakao.com", "kakaoAud"),
-    GOOGLE("GOOGLE", "구글", "https://accounts.google.com", "googleAud"),
-    APPLE("APPLE", "애플", "", "appleAud");
+    KAKAO("KAKAO", "카카오", "https://kauth.kakao.com"),
+    GOOGLE("GOOGLE", "구글", "https://accounts.google.com"),
+    APPLE("APPLE", "애플", "");
     
     private final String code;
     private final String description;
     // 자격증명 제공자 검증을 위한 필드
     private final String issUrl;
-    // 자격증명 제공자에 등록된 애플리케이션 아이디/
-    private final String aud;
     
     
     /**
--- src/main/java/com/LetMeDoWith/LetMeDoWith/service/AuthService.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/service/AuthService.java
@@ -1,57 +1,124 @@
 package com.LetMeDoWith.LetMeDoWith.service;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
-import org.springframework.stereotype.Service;
-
+import com.LetMeDoWith.LetMeDoWith.client.AuthClient;
 import com.LetMeDoWith.LetMeDoWith.dto.responseDto.CreateTokenRefreshResDto;
+import com.LetMeDoWith.LetMeDoWith.dto.responseDto.OidcPublicKeyResDto;
 import com.LetMeDoWith.LetMeDoWith.dto.valueObject.AccessTokenVO;
+import com.LetMeDoWith.LetMeDoWith.dto.valueObject.OidcPublicKeyVO;
 import com.LetMeDoWith.LetMeDoWith.entity.auth.RefreshToken;
+import com.LetMeDoWith.LetMeDoWith.enums.SocialProvider;
 import com.LetMeDoWith.LetMeDoWith.enums.common.FailResponseStatus;
 import com.LetMeDoWith.LetMeDoWith.exception.RestApiException;
 import com.LetMeDoWith.LetMeDoWith.provider.AuthTokenProvider;
 import com.LetMeDoWith.LetMeDoWith.repository.auth.RefreshTokenRedisRepository;
+import com.LetMeDoWith.LetMeDoWith.util.AuthUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-	
-	private final AuthTokenProvider authTokenProvider;
-
-	private final RefreshTokenRedisRepository refreshTokenRedisRepository;
-
-	public CreateTokenRefreshResDto createTokenRefresh(String accessToken, String refreshToken, String userAgent) {
-
-		Long memberId = authTokenProvider.validateToken(refreshToken, AuthTokenProvider.TokenType.RTK);
-
-		// Redis에서 RTK info 조회
-		RefreshToken savedRefreshToken = refreshTokenRedisRepository.findById(refreshToken)
-			.orElseThrow(() -> new RestApiException(FailResponseStatus.TOKEN_EXPIRED_BY_ADMIN));
-
-		// RTK 추가 보안 점검
-		if(!savedRefreshToken.getMemberId().equals(memberId)) {
-			throw new RestApiException(FailResponseStatus.INVALID_RTK_TOKEN_MEMBER_NOT_MATCHED);
-		} else if(!savedRefreshToken.getAccessToken().equals(accessToken)) {
-			throw new RestApiException(FailResponseStatus.INVALID_RTK_TOKEN_ATK_NOT_MATCHED);
-		}
-
-		// 신규 ATK 발급
-		AccessTokenVO accessTokenVO = authTokenProvider.createAccessToken(memberId);
-
-		// 신규 RTK 발급
-		RefreshToken newRefreshToken = authTokenProvider.createRefreshToken(memberId, accessTokenVO.token(), userAgent);
-
-		// 기존 RTK info Redis에서 삭제
-		refreshTokenRedisRepository.delete(savedRefreshToken);
-
-		return CreateTokenRefreshResDto.builder()
-			.accessToken(accessTokenVO.token())
-			.accessTokenExpireAt(accessTokenVO.expireAt())
-			.refreshToken(newRefreshToken.getToken())
-			.build();
-
-	}
-
+    
+    private final AuthTokenProvider authTokenProvider;
+    
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+    
+    private final SocialProviderAuthFactoryService socialProviderAuthFactoryService;
+    
+    @Value("${oidc.aud.kakao}")
+    private String KAKAO_AUD;
+    
+    @Value("${oidc.aud.google}")
+    private String GOOGLE_AUD;
+    
+    @Value("${oidc.aud.apple}")
+    private String APPLE_AUD;
+    
+    public CreateTokenRefreshResDto createTokenRefresh(String accessToken,
+        String refreshToken,
+        String userAgent) {
+        
+        Long memberId = authTokenProvider.validateToken(refreshToken,
+                                                        AuthTokenProvider.TokenType.RTK);
+        
+        // Redis에서 RTK info 조회
+        RefreshToken savedRefreshToken = refreshTokenRedisRepository.findById(refreshToken)
+                                                                    .orElseThrow(() -> new RestApiException(
+                                                                        FailResponseStatus.TOKEN_EXPIRED_BY_ADMIN));
+        
+        // RTK 추가 보안 점검
+        if (!savedRefreshToken.getMemberId().equals(memberId)) {
+            throw new RestApiException(FailResponseStatus.INVALID_RTK_TOKEN_MEMBER_NOT_MATCHED);
+        } else if (!savedRefreshToken.getAccessToken().equals(accessToken)) {
+            throw new RestApiException(FailResponseStatus.INVALID_RTK_TOKEN_ATK_NOT_MATCHED);
+        }
+        
+        // 신규 ATK 발급
+        AccessTokenVO accessTokenVO = authTokenProvider.createAccessToken(memberId);
+        
+        // 신규 RTK 발급
+        RefreshToken newRefreshToken = authTokenProvider.createRefreshToken(memberId,
+                                                                            accessTokenVO.token(),
+                                                                            userAgent);
+        
+        // 기존 RTK info Redis에서 삭제
+        refreshTokenRedisRepository.delete(savedRefreshToken);
+        
+        return CreateTokenRefreshResDto.builder()
+                                       .accessToken(accessTokenVO.token())
+                                       .accessTokenExpireAt(accessTokenVO.expireAt())
+                                       .refreshToken(newRefreshToken.getToken())
+                                       .build();
+        
+    }
+    
+    
+    /**
+     * Signature 검증이 완료돤 ID Token을 얻는다.
+     *
+     * @param token    검증하려는 인코딩된 ID token
+     * @param provider 자격증명 제공자
+     * @return 서명을 검증 완료한 ID Token.
+     */
+    public Jws<Claims> getVerifiedOidcIdToken(SocialProvider provider, String token) {
+        AuthClient client = socialProviderAuthFactoryService.getClient(provider);
+        
+        Mono<OidcPublicKeyResDto> publicKeyListInMono = client.getPublicKeyList();
+        OidcPublicKeyResDto publicKeyList = publicKeyListInMono.block();
+        String aud = getAudValueForProvider(provider);
+        
+        String kid = AuthUtil.getKidFromUnsignedTokenHeader(token, aud, provider.getIssUrl());
+        
+        OidcPublicKeyVO keyVO = publicKeyList.keys().stream()
+                                             .filter(key -> key.kid().equals(kid))
+                                             .findFirst()
+                                             .orElseThrow(IllegalArgumentException::new);
+        
+        return AuthUtil.verifyOidcToken(token, keyVO.n(), keyVO.e());
+    }
+    
+    private String getAudValueForProvider(SocialProvider provider) {
+        switch (provider) {
+            case APPLE -> {
+                return APPLE_AUD;
+            }
+            
+            case GOOGLE -> {
+                return GOOGLE_AUD;
+            }
+            
+            case KAKAO -> {
+                return KAKAO_AUD;
+            }
+            
+            default -> {
+                throw new RuntimeException();
+            }
+        }
+    }
+    
 }
