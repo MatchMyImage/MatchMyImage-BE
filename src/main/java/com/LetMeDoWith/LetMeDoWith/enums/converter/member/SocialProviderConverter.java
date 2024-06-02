aac4d018b5a0c0df4ef03909fbc03556797bd766
SJ Kim <hedro0458@gmail.com>
2024-06-02 22:00:52 +0900

    feat: [긴급] SocialProvider에 대한 Controller 요청 Converter

---
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/enums/converter/member/SocialProviderConverter.java
@@ -0,0 +1,11 @@
+package com.LetMeDoWith.LetMeDoWith.enums.converter.member;
+
+import com.LetMeDoWith.LetMeDoWith.enums.SocialProvider;
+import com.LetMeDoWith.LetMeDoWith.enums.converter.AbstractConverter;
+
+public class SocialProviderConverter extends AbstractConverter<SocialProvider> {
+    
+    public SocialProviderConverter() {
+        super(SocialProvider.class);
+    }
+}
