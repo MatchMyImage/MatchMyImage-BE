6e5d27f550c170e0e2471689410a32385a97db15
SJ Kim <hedro0458@gmail.com>
2024-06-07 14:02:59 +0900

    update: 회원가입 완료 엔드포인트를 Interceptor 제외 대상에 추가

--- src/main/java/com/LetMeDoWith/LetMeDoWith/config/WebConfig.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/config/WebConfig.java
@@ -1,35 +1,35 @@
 package com.LetMeDoWith.LetMeDoWith.config;
 
+import com.LetMeDoWith.LetMeDoWith.enums.converter.member.FollowTypeConverter;
+import com.LetMeDoWith.LetMeDoWith.interceptor.AuthenticateInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.LetMeDoWith.LetMeDoWith.enums.converter.member.FollowTypeConverter;
-import com.LetMeDoWith.LetMeDoWith.interceptor.AuthenticateInterceptor;
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-	private final AuthenticateInterceptor authenticateInterceptor;
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(authenticateInterceptor)
-			.addPathPatterns("/api/v1/**")
-			.excludePathPatterns(
-				"/health-check",
-				"/api/v1/auth/token",
-				"/api/v1/auth/token/refresh"
-				);
-			// TODO - 운영 반영 시 주석 해제
-			// .excludePathPatterns("/**"); // 개발 시, 테스트를 위해 actvice
-	}
-
-	@Override
-	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new FollowTypeConverter());
-	}
+    
+    private final AuthenticateInterceptor authenticateInterceptor;
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticateInterceptor)
+                .addPathPatterns("/api/v1/**")
+                .excludePathPatterns(
+                    "/health-check",
+                    "/api/v1/auth/token",
+                    "/api/v1/auth/token/refresh",
+                    "/api/v1/member"
+                );
+        // TODO - 운영 반영 시 주석 해제
+        // .excludePathPatterns("/**"); // 개발 시, 테스트를 위해 actvice
+    }
+    
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new FollowTypeConverter());
+    }
 }
