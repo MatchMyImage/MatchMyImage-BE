9c20c198da99823bf06ec05ef8c0ab081640b0d8
SJ Kim <hedro0458@gmail.com>
2024-08-13 23:44:17 +0900

    feat: 토큰 발급 요청에 대한 응답 모델 생성

--- src/main/java/com/LetMeDoWith/LetMeDoWith/application/auth/dto/AuthTokenVO.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/application/auth/dto/AuthTokenVO.java
@@ -1,7 +1,19 @@
 package com.LetMeDoWith.LetMeDoWith.application.auth.dto;
 
+import com.LetMeDoWith.LetMeDoWith.domain.auth.RefreshToken;
 import java.time.LocalDateTime;
 
 public record AuthTokenVO(String token, LocalDateTime expireAt) {
-
+    
+    /**
+     * Refresh Token을 AuthTokenVO 타입으로 변환
+     *
+     * @param rtk
+     * @return
+     */
+    public static AuthTokenVO fromRtk(RefreshToken rtk) {
+        return new AuthTokenVO(rtk.getToken(),
+                               LocalDateTime.now().plusSeconds(rtk.getExpireSec()));
+    }
+    
 }
