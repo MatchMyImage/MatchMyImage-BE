00cd29ceb8d7c8ab49dde291924b1029d122956d
SJ Kim <hedro0458@gmail.com>
2024-10-10 22:15:19 +0900

    feat: task 도메인 내 TaskCategory 생성 및 초기데이터 마이그레이션 스크립트 생성

---
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/domain/task/TaskCategory.java
@@ -0,0 +1,50 @@
+package com.LetMeDoWith.LetMeDoWith.domain.task;
+
+import com.LetMeDoWith.LetMeDoWith.common.entity.BaseAuditEntity;
+import com.LetMeDoWith.LetMeDoWith.common.enums.BaseEnum;
+import com.LetMeDoWith.LetMeDoWith.domain.AggregateRoot;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AggregateRoot
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskCategory extends BaseAuditEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false)
+    private TaskCategoryCreationType creationType;
+    
+    @Column(nullable = false)
+    private String emoji;
+    
+    @Column
+    private Long categoryHolderId;
+    
+    @Getter
+    @AllArgsConstructor
+    public enum TaskCategoryCreationType implements BaseEnum {
+        COMMON("COMMON", "공통"),
+        USER_CUSTOM("USER_CUSTOM", "유저 개인");
+        
+        private final String code;
+        private final String description;
+    }
+}
