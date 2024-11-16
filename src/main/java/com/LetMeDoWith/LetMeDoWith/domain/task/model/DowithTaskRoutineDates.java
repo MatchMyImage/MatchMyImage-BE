0fb728fc2849ca7003d0e1a530551fa004abd1e6
gyun_ky <kiyoon9390@gmail.com>
2024-11-17 04:29:25 +0900

    feat: 모델 수정 및 test 추가

--- src/main/java/com/LetMeDoWith/LetMeDoWith/domain/task/model/DowithTaskRoutineDates.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/domain/task/model/DowithTaskRoutineDates.java
@@ -1,22 +1,22 @@
 package com.LetMeDoWith.LetMeDoWith.domain.task.model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class DowithTaskRoutineDates {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class DowithTaskRoutineDate {
+
   private Set<LocalDate> dates;
 
-  public boolean isEqual(Set<LocalDate> dates) {
-    return this.dates.equals(dates);
+  public static DowithTaskRoutineDate of(Set<LocalDate> dates) {
+    return new DowithTaskRoutineDate(dates);
   }
+
 }
