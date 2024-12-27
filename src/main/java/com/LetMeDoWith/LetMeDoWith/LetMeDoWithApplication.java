44ba0415f6ac7432d52ba12c1d9ab75915e3f355
gyun_ky <kiyoon9390@gmail.com>
2024-12-27 22:47:47 +0900

    feat: swagger

--- src/main/java/com/LetMeDoWith/LetMeDoWith/LetMeDoWithApplication.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/LetMeDoWithApplication.java
@@ -1,5 +1,7 @@
 package com.LetMeDoWith.LetMeDoWith;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,6 +9,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerA
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "/", description = "Default Server")
+    }
+)
 @SpringBootApplication
 // @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class LetMeDoWithApplication {
